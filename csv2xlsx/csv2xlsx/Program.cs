074ce560a6f415908ae5679dd4dd529b2e5f599d
Stephen Wuebker <47507003+stephenwuebker@users.noreply.github.com>
2019-07-31 16:51:59 -0400

    Add files

---
+++ csv2xlsx/csv2xlsx/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Office.Interop.Excel;
+
+namespace csv2xlsx
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string csvPath = "";
+            string newFilename = "";
+
+            // make sure the correct arguments were passed
+            if (args.Count() > 2 || args.Count() < 1)
+            {
+                Console.WriteLine("csv2xlsx takes one or two parameters. Call csv2xlsx with the file you wish to convert and optionally where you want to save it.");
+                return;
+            }
+
+            csvPath = args[0].ToString();
+
+            if (args.Count() == 2)
+                newFilename = args[1].ToString();
+
+            // this might not be required. excel should be able to open anything -- maybe
+            //// make sure it's excel format
+            //if (!xlsPath.Substring(xlsPath.Length-3, 3).Equals("xls") || !xlsPath.Substring(xlsPath.Length-4, 4).Equals("xlsx"))
+            //{
+            //    Console.WriteLine("Invalid file type. Please enter an excel file type (.xls or .xlsx)");
+            //    return;
+            //}
+
+            SaveAs(csvPath, newFilename);
+
+
+        }
+
+
+        public static void SaveAs(string csvPath, string newFilename)
+        {
+            try
+            {
+                Application app = new Application();
+                Workbook wbWorkbook = app.Workbooks.Open(csvPath); //app.Workbooks.Add(Type.Missing);
+
+
+                if (newFilename == "")
+                    newFilename = csvPath.Substring(0, csvPath.LastIndexOf(".")) + ".xslx";
+
+                wbWorkbook.SaveAs(newFilename, Microsoft.Office.Interop.Excel.XlFileFormat.xlOpenXMLWorkbook,
+                    Type.Missing, Type.Missing, Type.Missing, Type.Missing, Microsoft.Office.Interop.Excel.XlSaveAsAccessMode.xlNoChange,
+                    Type.Missing, Type.Missing, Type.Missing, Type.Missing, Type.Missing);
+
+                wbWorkbook.Close(false, csvPath, true);
+                app.Quit();
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
---
+++ xls2csv/xls2csv/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Office.Interop.Excel;
+
+namespace xls2csv
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string xlsPath = "";
+            string newFilename = "";
+            string passWord = "";
+
+            // make sure the correct arguments were passed
+            if (args.Count() > 3 || args.Count() < 1)
+            {
+                Console.WriteLine("xls2csv takes one or two or three parameters. Call xls2csv with the file you wish to convert and optionally where you want to save it. If there is a password on the file, it is the third argument.");
+                return;
+            }
+
+            xlsPath = args[0].ToString();
+
+            if (args.Count() > 1)
+                newFilename = args[1].ToString();
+
+            if (args.Count() > 2)
+                passWord = args[2].ToString();
+
+            // this might not be required. excel should be able to open anything -- maybe
+            //// make sure it's excel format
+            //if (!xlsPath.Substring(xlsPath.Length-3, 3).Equals("xls") || !xlsPath.Substring(xlsPath.Length-4, 4).Equals("xlsx"))
+            //{
+            //    Console.WriteLine("Invalid file type. Please enter an excel file type (.xls or .xlsx)");
+            //    return;
+            //}
+
+            SaveAs(xlsPath, newFilename, passWord);
+
+
+        }
+
+
+        public static void SaveAs(string xlsPath, string newFilename, string passWord)
+        {
+            try
+            {
+                Application app = new Application();
+                Workbook wbWorkbook = app.Workbooks.Open(xlsPath, Type.Missing, Type.Missing, Type.Missing, passWord); //app.Workbooks.Add(Type.Missing);
+
+
+                if(newFilename == "")
+                    newFilename = xlsPath.Substring(0, xlsPath.LastIndexOf(".")) + ".csv";
+
+                wbWorkbook.SaveAs(newFilename, Microsoft.Office.Interop.Excel.XlFileFormat.xlCSVWindows, 
+                    Type.Missing, Type.Missing, Type.Missing, Type.Missing, Microsoft.Office.Interop.Excel.XlSaveAsAccessMode.xlNoChange, 
+                    Type.Missing, Type.Missing, Type.Missing, Type.Missing, Type.Missing);
+
+                wbWorkbook.Close(false, xlsPath, true);
+                app.Quit();
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
